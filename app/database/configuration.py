6316570ba1da3e63bf4231fc2aaca2b08c4c6133
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-17 19:31:27 -0500

    se crea conexion a bd

--- app/database/configuration.py
+++ app/database/configuration.py
@@ -1,3 +1,8 @@
+from sqlalchemy import create_engine,event
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+
+
 #datos para la conexion a BD
 
 dataBaseName="gestordb"
@@ -9,4 +14,9 @@ server="localhost"
 #creando la conexion
 dataBaseConnection=f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{connectionPort}/{dataBaseName}"
 
-print(dataBaseConnection)
+#creo el motor de conexion
+engine=create_engine(dataBaseConnection)
+
+#abrir la sesion con la bd
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
