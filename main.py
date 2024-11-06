a5a294d5979485c6a917179af39a87cb8b0a840e
Daniel Bustamante <danielbustamante1606@gmail.com>
2024-11-05 20:57:59 -0500

    dnlfeat conectamos y actualizamos app al API con datos

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from app.api.routes.endpoints import rutas
 from app.api.models.tablasSQL import Base
 
 from starlette.responses import RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+#Configurar el protocolo CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app = FastAPI()
 
